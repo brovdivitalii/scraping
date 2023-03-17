0b81d27944f8c5fa248a5416d5ff7de0e2c05875
Vitalii Brovdi <brovdi.vitaliy@gmail.com>
2023-03-17 10:33:40 +0200

    +email scrap

--- main.py
+++ main.py
@@ -29,10 +29,11 @@ with open("data.txt", "w", encoding='utf-8') as f:
         txt = faculty.find("h2").text
         # пошук посилання і додавання його у файл
         href = faculty.find("div", class_="details").find_all("p")[-1].find("span", class_="value").find("a").text
+        email = faculty.find("div", class_="details").find_all("p")[2].find("span", class_="value").find("a").text
         url = "https://" + href
         print(txt)
         print(url)
+        print(email)
         f.writelines(txt + "\n")
         f.writelines(url + "\n")
-
-GetPage()
+        f.writelines(email + "\n")
